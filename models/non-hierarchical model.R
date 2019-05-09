2e768524555357ea96f69f3f3330b52839fa4091
weinbergerlab <dweinber@gmail.com>
2019-05-08 22:17:59 -0400

    multinomial, non-hierarchical model

--- models/non-hierarchical model.R
+++ models/non-hierarchical model.R
@@ -1,19 +1,43 @@
 non_hierarchical_func<-function(){
-model_string<-"
+  model_string<-"
 model{
-for(i in 1:N){
-N_IPD[i]~dpois(lambda[i])
-log(lambda[i])<- (beta[1,st.index[i]] +beta[2,st.index[i]]*vax[i] +log(N_POP[i]))
-}
-
-for(j in 1:2){
-for(k in 1:13){
-beta[j,k]~dnorm(0, 1e-4)
-}
-}
-
+  
+  for(i in 1:2){
+  
+  #Likelihood
+  y[i, 1:14] ~ dmulti(p[i, 1:14], m[i])
+  
+  #Multionmial Logistic Regression    
+  for(j in 1:13){
+  p_temp[i,j] <- exp(beta0[j] + beta1[j]*vax[i])
+  }
+  for(j in 1:13){
+  p[i,j] <- p_temp[i,j]/(1 + sum(p_temp[i, 1:13]))
+  }
+  p[i,14] <- 1/(1 + sum(p_temp[i, 1:13]))
+  
+  }
+  
+  #Serotype-Specific Vaccine Effects
+  #100*(p_{no_vax} - p_{vax})/p_{no_vax}
+  for(j in 1:13){
+  sero_vax_effect[j] <- 100*(1 - p[2,j]/p[1,j])
+  }
+  
+  #Priors
+  for(j in 1:13){
+  
+  beta0[j] ~ dnorm(0, 0.0001)
+  beta1[j] ~ dnorm(0, 0.0001)
+  
+  }
+  
+  #Overall Vaccine Effect
+  overall_vax_effect <- 100*(1 - (1 - p[2,14])/(1 - p[1,14]))
+  
 }
 "
+
 
 ##############################################################
 #Model Fitting
@@ -21,47 +45,50 @@ beta[j,k]~dnorm(0, 1e-4)
 jags.inits1 <- function(){
   list(".RNG.seed"=c(1), ".RNG.name"='base::Wichmann-Hill')
 }
-model_jags<-jags.model(textConnection(model_string),n.chains=2,
+
+model_spec<-textConnection(model_string)
+model_jags<-jags.model(model_spec, 
                        inits = jags.inits1,
-                       data=list('N' = nrow(d1),
-                                 'st.index'=d1$st.index,
-                                 'N_IPD'=d1[,outcome.var],
-                                 'N_POP'=d1$pop,
-                                 'vax'=d1$vax
-                       )) 
+                       data=list('y' = y,
+                                 'm' = m,
+                                 'vax' = vax),
+                       n.adapt=10000, 
+                       n.chains=3)
 
-update(model_jags, 
-       n.iter=10000) 
+params<-c('overall_vax_effect',
+          'sero_vax_effect')
 
+
+##############################################
+#Posterior Sampling
+##############################################
 posterior_samples<-coda.samples(model_jags, 
-                                variable.names=c("beta"),
-                                thin=1,
-                                n.iter=50000)
+                                params, 
+                                n.iter=20000)
 
-post1.summary<-summary(posterior_samples)
-#plot(posterior_samples, 
-#     ask=TRUE)
-coefs<-post1.summary[['quantiles']][,c('2.5%','50%','97.5%')] 
-st.slp.rows<-grep('beta[2,', row.names(coefs), fixed=T)
+###################################################
+#POSTERIOR INFERENCE
+###################################################
+post_means<-colMeans(posterior_samples[[1]])
+ci<-t(hdi(posterior_samples[[1]], credMass = 0.95))
+ci<-round(ci,1)
+post_means<-round(post_means,1)
+
 st.labs<-as.character(unique(d1$st))
-post.all<-do.call(rbind,posterior_samples)
-#plot(post.all[,'beta[2,3]'], type='l') 
-#hist(post.all[,'beta[2,3]']) 
-prob.effect.st.u0<-apply(post.all[,st.slp.rows],2,function(x) mean(x<0))
-prob.effect.st.u0
-log.rr.st<-t(apply(post.all[,st.slp.rows],2,quantile, probs=c(0.025,0.5,0.975)))
 
-st.VE<- 100*round( 1 - exp(coefs[st.slp.rows,]) ,2)
+yrange<-range(ci)
 
-yrange<-range(st.VE)
+overall.VE<-c(post_means[1], ci[1,])
+st.VE<- cbind(post_means[-1], ci[-1,])
+
 
 #install.packages('rmeta')
 library(rmeta)
 summary_data <- 
   structure(list(
-    mean  = c(NA, NA,st.VE[,'50%']), 
-    lower = c(NA, NA,st.VE[,'97.5%']),
-    upper = c(NA, NA,st.VE[,'2.5%']),
+    mean  = c(NA, NA,post_means[2:14],NA,post_means[1]), 
+    lower = c(NA, NA,ci[2:14,1],NA,ci[1,1]),
+    upper = c(NA, NA,ci[2:14,2],NA,ci[1,2]),
     .Names = c("mean", "lower", "upper"), 
     row.names = c(NA, -11L), 
     class = "data.frame"))
@@ -70,11 +97,12 @@ if (outcome.var=='n_s11_pp'){
   st.VE[8,]<-NA
 }
 tabletext<-cbind(
-  c("", "Serotype", st.labs),
-  c("Vaccine", "(N=42240)", d1[,outcome.var][d1$vax==1]),
-  c("Control", "(N=42256)",  d1[,outcome.var][d1$vax==0]),
-  c("", "VE", paste0(st.VE[,'50%'],'% (', st.VE[,'97.5%'],'%, ',st.VE[,'2.5%'] ,'%' ,')'))
-  ) 
+  c("", "Serotype", st.labs, NA, "All"),
+  c("Vaccine", "(N=42240)", d1[,outcome.var][d1$vax==1], NA, sum(d1[,outcome.var][d1$vax==1])),
+  c("Control", "(N=42256)",  d1[,outcome.var][d1$vax==0], NA, sum(d1[,outcome.var][d1$vax==0])),
+  c("", "VE", paste0(post_means[2:14],'% (', ci[2:14,1],'%, ',ci[2:14,2] ,'%' ,')'), 
+    NA, paste0(post_means[1],'% (', ci[1,1],'%, ', ci[1,2],'%',')' ) )
+)
     
   
 res.list<-list('tabletext'=tabletext, 'summary_data'=summary_data,'st.VE'=st.VE)
