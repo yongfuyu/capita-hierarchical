c5d22a24f797e30dc6d9f4639fe81b6803aeec4e
weinbergerlab <dweinber@gmail.com>
2019-07-02 15:27:13 -0400

    add labels to plot comparing hierarchical and non-hierarchical

--- capita s11 non bacteremic.R
+++ capita s11 non bacteremic.R
@@ -42,7 +42,7 @@ ve.non.varying
 #####################################################################################
 source('./models/non-hierarchical model.R')
 nonhier1<-non_hierarchical_func()
-nonhier.st.eff<-nonhier1$st.VE
+nonhier.st.eff<-as.numeric(nonhier1$st.VE[,1])
 tiff('./results/non_hierarchical.tif', width=8, height=7,res=200,units = 'in')
 rmeta::forestplot(nonhier1$tabletext, 
                   mean=as.numeric(nonhier1$summary_data$mean),
@@ -61,7 +61,7 @@ dev.off()
 #####################################################################################
 source('./models/hierarchical model.R')
 hier1<-hierarchical.mod.func()
-hier.st.eff<-hier1$st.VE
+hier.st.eff<-as.numeric(hier1$st.VE[,1])
 
 tiff('./results/hierarchical.tif', width=8, height=7,res=200,units = 'in')
 rmeta::forestplot(hier1$tabletext, 
@@ -83,24 +83,32 @@ dev.off()
 ##COMPARE HIERARCHICAL VS NON-HIERARCHICAL
 tiff('./results/hierarchical vs non hierarchical xy.tif', width=8, height=8,res=200,units = 'in')
 par(mfrow=c(1,1))
-plot( nonhier.st.eff[,1],hier.st.eff[,1], ylim=c(-100,105), 
+plot( nonhier.st.eff,hier.st.eff, ylim=c(-100,105), 
       xlim=c(-100,105), bty='l', ylab='Hierarchical', xlab='non-hierarchical')
 #arrows(x0=hier.st.eff[,2], x1=hier.st.eff[,2], y0=nonhier.st.eff[,1],y1=nonhier.st.eff[,3], length=0 )
 #arrows(x0=hier.st.eff[,1], x1=hier.st.eff[,3], y0=nonhier.st.eff[,2],y1=nonhier.st.eff[,2], length=0 )
-text( nonhier.st.eff[,1],hier.st.eff[,1], unique(d1$st), adj=c(1,1), cex=0.5)
+text( nonhier.st.eff,hier.st.eff, unique(d1$st), adj=c(1,1), cex=0.5)
 abline(h=0,v=0, col='gray', lty=2)
 #abline(h=60, v=50)
 dev.off()
 
 tiff('./results/hierarchical vs non hierarchical shrinkage.tif', width=8, height=6,res=200,units = 'in')
-plot.nonhier.st.eff <- nonhier.st.eff[,1]
+plot.nonhier.st.eff <- nonhier.st.eff
 plot.nonhier.st.eff[plot.nonhier.st.eff< -10000]<-NA
 plot( plot.nonhier.st.eff,rep(2,13), ylim=c(1,2.1),yaxt='n',ylab='',xlab='Vaccine Efficacy', xlim=c(-200,100),col='white', bty='n')
 symbols(plot.nonhier.st.eff,rep(2,13),sqrt(st.n/pi), cex=0.2, inches=0.1, add=T)
 #points(hier.st.eff[,2],rep(1,13))
-symbols(hier.st.eff[,1],rep(1,13),sqrt(st.n/pi), cex=0.2, inches=0.1, add=T)
-arrows(x0=plot.nonhier.st.eff , x1=hier.st.eff[,1], y0=2, y1=1, length=0, lty=2, col='gray')
+symbols(hier.st.eff,rep(1,13),sqrt(st.n/pi), cex=0.2, inches=0.1, add=T)
+arrows(x0=plot.nonhier.st.eff , x1=hier.st.eff, y0=2, y1=1, length=0, lty=2, col='gray')
 text(-145,2, 'Non-hierarchical', pos=2)
+jitter.plot.nonhier.st.eff<-plot.nonhier.st.eff
+jitter.y<-rep(2.01, length(jitter.plot.nonhier.st.eff))
+jitter.y[unique(d1$st)=='23F']<-2.04
+jitter.y[unique(d1$st)=='19F']<-1.98
+jitter.y[unique(d1$st)=='5']<-2.04
+jitter.y[unique(d1$st)=='19A']<-2.06
+jitter.y[unique(d1$st)=='3']<-2.03
+text(jitter.plot.nonhier.st.eff,jitter.y,  unique(d1$st), pos=3, xpd=NA, cex=0.75)
 text(-145,1, 'Hierarchical', pos=2)
 abline(v=0) 
 dev.off()
